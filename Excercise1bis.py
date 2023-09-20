03a38ab9c305eda86c91718ba1b02f7068070266
dmunozpa <93115743+dmunozpa@users.noreply.github.com>
2023-09-20 19:02:20 +0200

    T1 - Exercices

---
+++ Excercise1bis.py
@@ -0,0 +1,17 @@
+## Sum of even numbers: Write a program that sums all the even numbers from 1 to 100.
+def calculation():
+    # Method 2: Using a while loop
+    result = #TODO
+    number = #TODO
+    while number <= #TODO
+        result += number
+        number += #TODO
+
+    print("Sum of even numbers (Method 2):", result)
+
+
+def main():
+    calculation()
+
+if __name__ ==  "__main__":
+    main()
