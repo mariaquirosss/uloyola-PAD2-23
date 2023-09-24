1f5ef9be9390483063e595c91ddc9042243129d0
Daniel Muñoz <dmunoz@uloyola.es>
2023-09-24 13:01:56 +0200

    Updatating Exercise1bis with the TODO

--- Excercise1bis.py
+++ Excercise1bis.py
@@ -1,11 +1,11 @@
 ## Sum of even numbers: Write a program that sums all the even numbers from 1 to 100.
 def calculation():
     # Method 2: Using a while loop
-    result = #TODO
-    number = #TODO
-    while number <= #TODO
+    result = # TODO: Inizialize variable result
+    number = # TODO:  Inizialize variable result
+    while number <= # TODO: Generate the proper while loop
         result += number
-        number += #TODO
+        number += # TODO: Update number variable for even.
 
     print("Sum of even numbers (Method 2):", result)
 
