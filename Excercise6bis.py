ea7bc96be74d98bd4b1e37c828ad0a69a207d792
Daniel Muñoz <dmunoz@uloyola.es>
2023-09-24 12:25:20 +0200

    Adding bmi calculation and exception

--- Excercise6bis.py
+++ Excercise6bis.py
@@ -8,22 +8,27 @@
 def calculate_bmi(weight_kg, height_m):
     try:
         # Calculate BMI using the formula: BMI = weight (kg) / (height (m) * height (m))
-        #ToDo
+        bmi = weight_kg / (height_m * height_m)
         return bmi
     except ZeroDivisionError:
-        return "Height cannot be zero. Please enter a valid height."
+        return print("Height cannot be zero. Please enter a valid height.")
 
 def main():
+
     print("Welcome to the BMI Calculator!")
-    weight_kg = float(input("Enter your weight in kilograms: "))
-    height_m = float(input("Enter your height in meters: "))
+    try: 
+        weight_kg = float(input("Enter your weight in kilograms: "))
+        height_m = float(input("Enter your height in meters: "))
 
-    bmi = calculate_bmi(weight_kg, height_m)
+        bmi = calculate_bmi(weight_kg, height_m)
 
-    if isinstance(bmi, float):
-        print(f"Your BMI is: {bmi:.2f}")
-    else:
-        print(bmi)
+        if isinstance(bmi, float):
+            print(f"Your BMI is: {bmi:.2f}")
+        else:
+            print(bmi)
+    
+    except ValueError:
+        return print("Values must to be a float. Please enter a valid value.")
 
 if __name__ == "__main__":
     main()
