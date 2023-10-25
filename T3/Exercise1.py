66f7f425c6747f35591f121061cb1a07848c9836
Daniel Muñoz <dmunoz@uloyola.es>
2023-10-25 19:14:22 +0200

    Exercises 1 and 3

---
+++ T3/Exercise1.py
@@ -0,0 +1,64 @@
+class Person:
+    def __init__(self, name, surname, age, address, phone_number):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.address = address
+        self.phone_number = phone_number
+
+    def __del__(self):
+        print("Destructor called")
+
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_surname(self):
+        return self.surname
+
+    def set_surname(self, surname):
+        self.surname = surname
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+    def get_address(self):
+        return self.address
+
+    def set_address(self, address):
+        self.address = address
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    def print_info(self):
+        print(f"Name: {self.name}")
+        print(f"Surname: {self.surname}")
+        print(f"Age: {self.age}")
+        print(f"Address: {self.address}")
+        print(f"Phone Number: {self.phone_number}")
+
+# Example usage of the Person class:
+person = Person("Daniel", "M", 30, "123 Main St", "555-123-4567")
+person1 = Person("Daniel", "M", 30, "123 Main St", 123456)
+
+person.print_info()
+person1.print_info()
+
+person.__sizeof__
+
+
+person1.__del__()
+
+# You can also use the getters and setters to access and modify attributes:
+person.set_age(35)
+print(f"Updated Age: {person.get_age()}")
