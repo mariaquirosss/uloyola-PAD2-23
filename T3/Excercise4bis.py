1dccff6c508a306f61ccd6d7362351eb015e5359
Daniel Muñoz <dmunoz@uloyola.es>
2023-10-26 19:18:19 +0200

    Todo exercise4

--- T3/Excercise4bis.py
+++ T3/Excercise4bis.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class Figure(ABC):
     def __init__(self):
-        self.area = None
+        self.area = 10
         self.perimeter = None
 
     @abstractmethod
@@ -13,11 +13,22 @@ class Figure(ABC):
     def calculate_perimeter(self):
         pass
 
+class Square(Figure):
+    def __init__(self, #TODO):
+        
+
+    def calculate_area(self):
+        #TODO
+
+    def calculate_perimeter(self):
+        #TODO
+
+
 class Rectangle(Figure):
     def __init__(self, width, height):
         super().__init__()
-        self.width = width
         self.height = height
+        self.width = width
 
     def calculate_area(self):
         self.area = self.width * self.height
@@ -30,8 +41,3 @@ rectangle = Rectangle(5, 7)
 rectangle.calculate_area()
 rectangle.calculate_perimeter()
 print(f"Rectangle - Area: {rectangle.area}, Perimeter: {rectangle.perimeter}")
-
-circle = Circle(4)
-circle.calculate_area()
-circle.calculate_perimeter()
-print(f"Circle - Area: {circle.area}, Perimeter: {circle.perimeter}")
