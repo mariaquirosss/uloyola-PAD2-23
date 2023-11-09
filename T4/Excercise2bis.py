b663d508003755ca652f4f5382d58e60e2436aaf
Daniel Muñoz <dmunoz@uloyola.es>
2023-11-09 18:21:34 +0100

    Updateing Excersice

--- T4/Excercise2bis.py
+++ T4/Excercise2bis.py
@@ -6,7 +6,7 @@ class Point:
         # TODO
 
     @property
-    def x(self):
+    def x(self) -> float:
         # TODO
 
     @x.setter
@@ -14,7 +14,7 @@ class Point:
         # TODO
 
     @property
-    def y(self):
+    def y(self) -> float:
         return self._y
 
     @y.setter
@@ -30,10 +30,16 @@ class Point:
     def __str__(self):
         return f"({self._x}, {self._y})"
 
-# Test program
-point = Point(3, 4)
-print("Point:", point)
-print("X Coordinate:", point.x)
+def main():
 
-point.x = 0
-print("After setting X coordinate to 0:", point)
+    # Test program
+    point = Point(2, 3)
+    print("Point:", point)
+    print("Value X Coordinate:", point.x)
+
+    point.x = 0
+    print("X coordinate updated to 0:", point)
+
+if __name__ ==  "__main__":
+    main()
+
